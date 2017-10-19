a87e14d00f6de88f2695683248dd6d733cad2aea
millzhang <876753183@qq.com>
2017-10-19 14:10:05 +0800

    提交七牛插件

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # timepack-qiniu
+
+### How to Use?
+
+```
+npm install timepack-qiniu
+```
+
+> When Use method `fileUploader` in `timepack-util`,please import `timepack-qiniu` first
---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Timepack Qiniu package
+ * @author MillZhang
+ * @date 2017-2-17 13:58:23
+ * @version 1.0.1
+ */
+import moxie from './qiniu/moxie.js'
+import plupload from './qiniu/plupload.full.min.js'
+import qiniu from './qiniu/qiniu.js'
